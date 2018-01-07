22f020b3c8dd1edf62cba102a0ed4e269789e2b7
Caio <cvmartins1996@gmail.com>
2018-01-07 18:51:16 -0200

    Docker

--- pyramid/app.py
+++ pyramid/app.py
@@ -5,12 +5,12 @@ from pyramid.response import Response
 #### Primeiro programa com Pyramid
 
 def app(request):
-    return Response("Olá %(nome)s" % request.matchdict)
+    return Response("Ola %(nome)s" % request.matchdict)
 
 if __name__ == '__main__':
     with Configurator() as configura:
         configura.add_route('/', '{nome}')
-        configura.add_view(app, route_name='ola')
+        configura.add_view(app, route_name='/')
         programa = configura.make_wsgi_app()
     servidor = make_server('0.0.0.0', 8089, programa)
     servidor.serve_forever()
